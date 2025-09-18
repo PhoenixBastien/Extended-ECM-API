0caccd9acbbab69085ad3eac63c71fb944453304
Phoenix Bastien <91162508+PhoenixBastien@users.noreply.github.com>
2025-09-18 09:43:51 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,56 +1,33 @@
-import logging
-import os
 from threading import Thread
 
-from dotenv import load_dotenv
-import urllib3
-from pyxecm import OTCS
-
+from config import gcdocs
+from gcdocs import GCdocs
 from tasks import *
 
-logging.basicConfig(filename='app.log', filemode='w+', level=logging.DEBUG)
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-if load_dotenv():
-    hostname = os.getenv('OTCS_HOSTNAME')
-    username = os.getenv('OTCS_USERNAME')
-    password = os.getenv('OTCS_PASSWORD')
-else:
-    print('.env file not found')
-    quit()
-
-otcs = OTCS(
-    protocol='https',
-    hostname=hostname,
-    port=443,
-    public_url=hostname,
-    username=username,
-    password=password
-)
-
-otcs.authenticate()
-quit()
-
-root_id = 33266495
-root = otcs.get_node(node_id=root_id)
-total_subnodes = otcs.get_result_value(response=root, key='size')
+# personnel records
+root_id = 30713183
 page_size = 100
+
+root = gcdocs.get_node(node_id=root_id)
+total_subnodes = gcdocs.get_result_value(response=root, key='size')
 total_pages = (total_subnodes + page_size - 1) // page_size
 
-for page in range(1, total_pages + 1):
-    print('page', page)
-    nodes = otcs.get_subnodes(parent_node_id=root_id, page=page)
+for page in range(50, total_pages + 1):
+    print(f'page {page}/{total_pages}')
+    nodes = gcdocs.get_subnodes(
+        parent_node_id=root_id,
+        filter_node_types=GCdocs.ITEM_TYPE_BUSINESS_WORKSPACE,
+        limit=page_size,
+        page=page
+    )
+    node_ids = gcdocs.get_result_values(response=nodes, key='id')
     threads: list[Thread] = []
 
-    for i in range(len(nodes['results'])):
-        subtype = otcs.get_result_value(response=nodes, key='type', index=i)
-
-        if subtype != OTCS.ITEM_TYPE_BUSINESS_WORKSPACE:
-            continue
-
-        node_id = otcs.get_result_value(response=nodes, key='id', index=i)
-        t = Thread(target=task2, args=(otcs, node_id))
-        threads.append(t)
+    for node_id in node_ids:
+        threads.append(Thread(
+            target=gcdocs.assign_permission,
+            args=(node_id, GCdocs.PERMISSION_TYPES, 'custom', 31897943, 2)
+        ))
 
     for t in threads:
         t.start()
